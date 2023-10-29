84829fc4bde020a9e7d687455b71c294c5210f11
Disha Pant <64821756+dishap22@users.noreply.github.com>
2023-10-29 13:21:51 +0530

    Update Profile

--- website/read_json.py
+++ website/read_json.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template
+import requests
 
 json_file_path = "package.json"
 
@@ -13,9 +14,33 @@ except json.JSONDecodeError as e:
 
 app = Flask(__name__)
 
+def get_codeforces_rating(handle):
+    api_url = f"https://codeforces.com/api/user.info?handles={handle}"
+
+    try:
+        response = requests.get(api_url)
+        data = response.json()
+
+        if data['status'] == 'OK':
+            user_info = data['result'][0]
+            rating = user_info['rating']
+            return rating
+        else:
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
 @app.route('/')
 def index():
-    return render_template(r'profile.html', occupation=data['occupation'], name=data['full_name'], imageURL=data['profile_pic_url'], location=(data['city'] + ', ' + data['country']), experiences=data.get('experiences', []))
+    skills = data.get("skills", [])
+    user_handle = "vignesh_x"
+    rating = get_codeforces_rating(user_handle)
+
+    if rating is not None:
+        skills.append(f"Codeforces Rating: {rating}")
+
+    return render_template(r'profile.html', occupation=data['occupation'], name=data['full_name'], imageURL=data['profile_pic_url'], location=(data['city'] + ', ' + data['country']), skills=skills, experiences=data.get('experiences', []), education=data.get('education', []))
 
 if __name__ == '__main__':
     app.run()
